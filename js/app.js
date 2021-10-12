b83e754b0ee52b5c13a1f338ac203283c908884e
web-sarang <77715656+web-sarang@users.noreply.github.com>
2021-10-12 16:03:25 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,36 +1,3 @@
-//Tabs
-
-let tab = function () {
-    let tabNav = document.querySelectorAll('.tabs-nav__item, tab-icon, tab-btn'),
-        tabContent = document.querySelectorAll('.tab'),
-        tabName;
-
-    tabNav.forEach(item => {
-        item.addEventListener('click', selectTabNav)
-    });
-
-    function selectTabNav() {
-        tabNav.forEach(item => {
-            item.classList.remove('active');
-        });
-        this.classList.add('active');
-        tabName = this.getAttribute('data-tab-name');
-        selectTabContent(tabName);
-    }
-
-    function selectTabContent(tabName) {
-        tabContent.forEach(item => {
-            item.classList.contains(tabName) ? item.classList.add('active') : item.classList.remove('active');
-        })
-    }
-
-};
-
-
-tab();
-
-
-
 // Wizzard Nav
 const allItems = document.querySelectorAll(".bullet");
 
